81e116c532083f10e5a85e3b9dab6d19f6ca7487
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-06-19 08:28:10 +0545

    added category modal and migration

--- database/migrations/tenant/2023_06_19_020202_create_categories_table.php
+++ database/migrations/tenant/2023_06_19_020202_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
+            $table->text('image')->nullable();
             $table->timestamps();
         });
     }
