6f912536b00d56ed02aa970c2a4d971c1e5d6ccb
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-06-29 09:36:41 +0545

    delete image and updateimage in image service

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -2,16 +2,50 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
 class ImageService
 {
-    public static function upload($file, $path = "/")
+    public static function uploadImage($file, $path = "/")
     {
-        if (substr($path, 0, 1) != "/") {
-            $path = "/" . $path;
-        }
-        $fileName = time() . '.' . $file->getClientOriginalExtension();
-        $file->move(public_path('images' . $path), $fileName);
+        $data = $file;
+        list($type, $data) = explode(';', $data);
+        list(, $data) = explode(',', $data);
+        $data = base64_decode($data);
+        $extension = explode('/', $type)[1];
 
-        return url('images' . $path . '/' . $fileName);
+        $fileName = time() . Str::random(10) . '.' . $extension;
+
+        Storage::disk('public')->put($path . '/' . $fileName, $data);
+
+        // give full url of image  if tenant then add tenant name in url
+        return Storage::disk('public')->url($path . '/' . $fileName);
+    }
+
+    public static function deleteImage($file)
+    {
+        if ($file) {
+            if (filter_var($file, FILTER_VALIDATE_URL)) {
+                $file = explode('storage/', $file)[1];
+            }
+            Storage::disk('public')->delete($file);
+        }
+
+        return true;
+    }
+
+    public static function updateImage($file, $path, $oldFile)
+    {
+        if ($oldFile) {
+            $oldFile = explode('storage/', $oldFile)[1];
+
+            self::deleteImage($oldFile);
+        }
+
+        if (filter_var($file, FILTER_VALIDATE_URL)) {
+            return $file;
+        }
+        return self::uploadImage($file, $path ?? '/');
     }
 }
