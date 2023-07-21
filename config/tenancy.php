3b5cd4387055ec05efa1327865ac1d4c90e167cc
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-07-21 22:46:51 +0545

    updated tenant db prefix

--- config/tenancy.php
+++ config/tenancy.php
@@ -50,7 +50,7 @@ return [
          * Tenant database names are created like this:
          * prefix + tenant_id + suffix.
          */
-        'prefix' => 'mecom_',
+        'prefix' => 'mecomm_',
         'suffix' => '',
 
         /**
