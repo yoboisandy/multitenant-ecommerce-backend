34759a66d0167bbd8e2feced86ead694e43279bd
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-08-12 21:26:36 +0545

    create OrderProduct on order create

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Http\Resources\ProductResource;
+use App\Models\OrderProduct;
 use App\Repositories\OrderRepository;
 
 class OrderService extends BaseService
@@ -20,34 +21,44 @@ class OrderService extends BaseService
 
     public function createOrder($data)
     {
-        $products = [];
-        foreach ($data['products'] as $product) {
-            $item = $this->productService->getProduct($product['id']);
-            $products[] = [
-                'product' => new ProductResource($item),
-                'variant' => $variant = !empty($product['variant_id']) ? $item->variants?->where('id', $product['variant_id'])->first() ?? null : null,
-                'quantity' => $quantity = $product['quantity'],
-                'price' => ($variant ? $variant->selling_price : $item->selling_price) * $quantity,
+        return $this->withTransaction(function () use ($data) {
+            $products = [];
+            foreach ($data['products'] as $product) {
+                $item = $this->productService->getProduct($product['id']);
+                $products[] = [
+                    'product' => new ProductResource($item),
+                    'variant' => $variant = !empty($product['variant_id']) ? $item->variants?->where('id', $product['variant_id'])->first() ?? null : null,
+                    'quantity' => $quantity = $product['quantity'],
+                    'price' => ($variant ? $variant->selling_price : $item->selling_price) * $quantity,
+                ];
+            }
+            $data['products'] = $products;
+
+            $data['order_number'] = 1000 + $this->orderRepository->count();
+
+            $storeId = tenant('store_id');
+            $data['delivery_charge'] = tenancy()->central(function () use ($storeId) {
+                return $this->storeService->getStoreById($storeId)->setting?->delivery_charge ?? 0;
+            });
+
+            $data['total_price'] = collect($products)->sum('price') + $data['delivery_charge'];
+            $data['total_quantity'] = collect($products)->sum('quantity');
+
+            $order = $this->orderRepository->create($data);
+
+            foreach ($products as $product) {
+                OrderProduct::create([
+                    'order_id' => $order->id,
+                    'product_id' => $product['product']->id,
+                    'variant_id' => $product['variant']?->id,
+                ]);
+            }
+
+            return [
+                "quantity" => $data['total_quantity'],
+                "price" => $data['total_price'],
             ];
-        }
-        $data['products'] = $products;
-
-        $data['order_number'] = 1000 + $this->orderRepository->count();
-
-        $storeId = tenant('store_id');
-        $data['delivery_charge'] = tenancy()->central(function () use ($storeId) {
-            return $this->storeService->getStoreById($storeId)->setting?->delivery_charge ?? 0;
         });
-
-        $data['total_price'] = collect($products)->sum('price') + $data['delivery_charge'];
-        $data['total_quantity'] = collect($products)->sum('quantity');
-
-        $this->orderRepository->create($data);
-
-        return [
-            "quantity" => $data['total_quantity'],
-            "price" => $data['total_price'],
-        ];
     }
 
     public function getAllOrders()
