df012c0d570dfce1f98649070c4110110a10de84
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-11-17 18:08:09 +0545

    added a /welcome route

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
 Route::get('stripe-webhook', function (Request $request) {
     logger('Stripe webhook payload:', $request->all());
     return response()->json(['message' => 'success']);
